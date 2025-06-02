3683eb0b9ed58e54c02d3a17ddb926ab77beeafa
Yuskus <yuskus@yandex.ru>
2025-06-02 12:20:37 +0300

    add ef-migrator batch script

---
+++ batch scripts/ef-migrator.bat
@@ -0,0 +1,65 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+set "current_dir=%cd%"
+set "db_conn_value="
+set "program_directory="
+set "migrations_directory="
+
+for /r %%a in (*) do (
+    if "%%~nxa"=="docker-compose.yml" (
+        for /f "tokens=*" %%l in ('findstr /i "DB_CONN" "%%~nxa"') do (
+            set "db_conn_value=%%l"
+            set "db_conn_value=!db_conn_value:~0!"
+            goto :next_step
+        )
+    )
+)
+
+:next_step
+if not defined db_conn_value (
+    echo DB_CONN not found in any docker-compose.yml file.
+    exit /b 1
+)
+
+for /r %%a in (*) do (
+    if "%%~nxa"=="Program.cs" (
+        set "program_directory=%%~dpa"
+        goto :next_step2
+    )
+)
+
+:next_step2
+if not defined program_directory (
+    echo Program.cs not found in any directory.
+    exit /b 1
+)
+
+for /d /r %%d in (Migrations) do (
+    if exist "%%d*" (
+        set "migrations_directory=%%~dpd"
+        goto :end
+    )
+)
+
+:end
+if not defined migrations_directory (
+    echo Migrations directory not found in any directory.
+    exit /b 1
+)
+
+set "result_db_conn=!db_conn_value:~9!"
+set "result_program_directory=.!program_directory:%current_dir%=!"
+set "result_migrations_directory=.!migrations_directory:%current_dir%=!"
+
+echo ef-migrator found your parameters:
+echo connection string: !result_db_conn!
+echo program directory: !result_program_directory!
+echo migrations folder: !result_migrations_directory!
+
+echo here is your strings (you can copy it):
+echo dotnet ef migrations add "______" -s "!result_program_directory!" -p "!result_migrations_directory!"
+echo dotnet ef database update -s "!result_program_directory!" -p "!result_migrations_directory!" --connection "!result_db_conn!"
+
+endlocal
