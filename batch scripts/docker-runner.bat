199e33095c4b271c571839439f8b66314d205b54
Yuskus <yuskus@yandex.ru>
2025-06-06 09:41:55 +0300

    add to docker-runner docker-desktop autorun

--- batch scripts/docker-runner.bat
+++ batch scripts/docker-runner.bat
@@ -2,6 +2,23 @@
 
 setlocal enabledelayedexpansion
 
+set "docker_path=C:\Program Files\Docker\Docker\Docker Desktop.exe"
+
+tasklist /FI "IMAGENAME eq Docker Desktop.exe" | find /I "Docker Desktop.exe" >nul
+
+if %errorlevel%==0 (
+    goto :check-docker
+) else (
+    goto :run-docker
+)
+
+:run-docker
+if exist "%docker_path%" (
+    start "" "%docker_path%"
+    timeout /t 10 >nul
+)
+
+:check-docker
 docker info >nul 2>&1
 if %errorlevel% neq 0 (
     echo docker-runner: docker is not running. please start docker and try again.
@@ -11,16 +28,16 @@ if %errorlevel% neq 0 (
 echo docker-runner: started
 
 echo docker-runner: executing dotnet build
-dotnet build
+dotnet build || (echo dotnet build failed & exit /b 1)
 
 echo docker-runner: executing docker compose down
-docker-compose down
+docker-compose down || (echo docker-compose down failed & exit /b 1)
 
 echo docker-runner: executing docker compose build (without cache)
-docker-compose build --no-cache
+docker-compose build --no-cache || (echo docker-compose build failed & exit /b 1)
 
 echo docker-runner: executing docker compose up
-docker-compose up -d
+docker-compose up -d || (echo docker-compose up failed & exit /b 1)
 
 echo docker-runner: ended
 
