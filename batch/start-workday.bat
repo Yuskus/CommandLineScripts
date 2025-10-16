7237addb1ee4e21fb21cb87002e9414868d69e63
Yuskus <yuskus@yandex.ru>
2025-10-16 17:41:45 +0300

    fix start-workday (array)

--- batch/start-workday.bat
+++ batch/start-workday.bat
@@ -3,31 +3,30 @@
 setlocal enabledelayedexpansion
 
 :: paths (to .exe files or .lnk)
-set "docker_path=C:\Program Files\Docker\Docker\Docker Desktop.exe"
-set "linkpad_path=________\LINQPad8.exe"
-set "beaver_path=________\dbeaver.exe"
-set "obsidian_path=________\Obsidian.exe"
-set "telegram_path=________\Telegram.lnk"
-set "visualstudio_path=________\devenv.exe"
+set "paths[0]=______\Docker Desktop.exe"
+set "paths[1]=______\LINQPad8.exe"
+set "paths[2]=______\dbeaver.exe"
+set "paths[3]=______\Obsidian.exe"
+set "paths[4]=______\Telegram.lnk"
+set "paths[5]=______\devenv.exe"
 
 :: names (to .exe files)
-set "docker_name=Docker Desktop.exe"
-set "linkpad_name=LINQPad8.exe"
-set "beaver_name=dbeaver.exe"
-set "obsidian_name=Obsidian.exe"
-set "telegram_name=Telegram.exe"
-set "visualstudio_name=devenv.exe"
+set "names[0]=Docker Desktop.exe"
+set "names[1]=LINQPad8.exe"
+set "names[2]=dbeaver.exe"
+set "names[3]=Obsidian.exe"
+set "names[4]=Telegram.exe"
+set "names[5]=devenv.exe"
+
+set count=5
 
 :: for before running docker desktop
 wsl --update
 
 :: running
-call :check_and_run "%docker_name%" "%docker_path%"
-call :check_and_run "%linkpad_name%" "%linkpad_path%"
-call :check_and_run "%beaver_name%" "%beaver_path%"
-call :check_and_run "%obsidian_name%" "%obsidian_path%"
-call :check_and_run "%telegram_name%" "%telegram_path%"
-call :check_and_run "%visualstudio_name%" "%visualstudio_path%"
+for /L %%i in (0,1,!count!) do (
+    call :check_and_run "!names[%%i]!" "!paths[%%i]!"
+)
 
 echo all programs are running.
 
